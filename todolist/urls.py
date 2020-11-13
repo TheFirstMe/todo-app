456c390775faaea5dfc4914f4661b9d211db110d
Sourabh <sourabh@qburst.com>
2020-11-13 12:38:37 +0530

    Added complete functionality

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.addTodoItem, name='add'),
+    path('complete/<todo_id>', views.completedTodo, name='complete'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Todolist
+
+from django.views.decorators.http import require_POST
+
+from .forms import TodoListForm
 
 # Create your views here.
 
 
 def index(request):
     todo_items = Todolist.objects.order_by('id')
-    context = {'todo_items': todo_items}
+    form = TodoListForm()
+    # print(form.)
+    context = {'todo_items': todo_items, 'form': form}
     return render(request, 'todolist/index.html', context=context)
+
+
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        todo_item = Todolist(text=request.POST['text'])
+        todo_item.save()
+
+    return redirect('index')
+
+
+def completedTodo(request, todo_id):
+    todo = Todolist.objects.get(pk=todo_id)
+    todo.completed = not todo.completed
+    todo.save()
+
+    return redirect('index')
